2901978c25e678997f870d0647bd0b523ac3df22
mamunur1296 <programer597@gmail.com>
2022-09-03 05:10:52 +0600

    home page recape

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const makeRequest = async (url) => {
     const res = await fetch(url);
     if (!res.ok) {
@@ -17,6 +16,7 @@ const getData = () => {
 const displayData = (dataa) => {
     const newses = dataa.data.news_category;
     const setCatagory = document.getElementById('set-catagory');
+    console.log(newses.length);
     const categoryNamees = [];
     newses.forEach(news => {
         if (categoryNamees.indexOf(news.category_name) === -1) {
@@ -97,6 +97,100 @@ const displayBreakingNews = allnews => {
         mainContainer.appendChild(maindiv)
     })
 }
+
+
+
+
+
+const homePage = () => {
+    fetch('https://openapi.programming-hero.com/api/news/category/01')
+        .then(res => res.json())
+        .then(data => newsDisplay(data))
+}
+homePage();
+
+const newsDisplay = (allnews) => {
+    const newses = allnews.data;
+    const mainContainer = document.getElementById('main-container');
+    mainContainer.innerHTML = '';
+    const sarceRejult = document.getElementById('sarce-rejult').innerText = newses.length;
+    console.log(sarceRejult);
+    console.log(mainContainer.innerHTML);
+    newses.forEach(news => {
+        const maindiv = document.createElement("div");
+        maindiv.classList = ('row my-5');
+        maindiv.innerHTML = `
+                    <div class="col-md-3">
+                        <img src="${news.image_url ? news.image_url : "https://i.ibb.co/g9CSkZQ/image.png "}"
+                            class="img-fluid my-img rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-9">
+                        <div class="card-body">
+                            <h5 class="card-title">${news.title}</h5>
+                            <p class="card-text">${news.details.length > 10 ? news.details.slice(0, 150) : "NO details"}</p>
+                            <p class="card-text">${news.details.length > 150 ? news.details.slice(150, 400) + '...' : "NO details"}</p>
+                            
+                            <div class="d-flex align-items-center justify-content-between mt-5">
+                                <div class="d-flex align-items-center">
+                                    <div>
+                                        <a class="navbar-brand " href="#">
+                                            <img class="rounded-circle"
+                                                src="${news.author.img ? news.author.img : "https://i.ibb.co/g9CSkZQ/image.png "}"
+                                                alt="" width="40" height="40">
+                                        </a>
+                                    </div>
+                                    <div>
+                                        <div>${news.author.name ? news.author.name : "No data"}</div>
+                                        <div>${news.author.published_date ? news.author.published_date : "No data"}</div>
+                                    </div>
+                                </div>
+                                <div class="d-flex align-items-center">
+                                    <div class="h5">
+                                        <i class="fa-regular fa-eye"></i>
+                                    </div>
+                                    <div class="ms-2">
+                                        <h5>${news.total_view ? news.total_view : "No data"}</h5>
+                                    </div>
+                                </div>
+                                <div>
+                                    <i class="fa-solid fa-star-half-stroke"></i>
+                                    <i class="fa-regular fa-star"></i>
+                                    <i class="fa-regular fa-star"></i>
+                                    <i class="fa-regular fa-star"></i>
+                                    <i class="fa-regular fa-star"></i>
+                                </div>
+                                <div>
+                                    <button onclick="modalBodyById('${news._id}')" type="button" class="btn " data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                        <i class="fa-solid fa-arrow-right"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        `;
+        mainContainer.appendChild(maindiv)
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const modalBodyById = (id) => {
     makeRequest(`https://openapi.programming-hero.com/api/news/${id}`)
