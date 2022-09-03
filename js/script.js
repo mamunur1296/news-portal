2a5aaeaff085b3a27523bfece8535ab618c35b57
mamunur1296 <programer597@gmail.com>
2022-09-03 08:40:17 +0600

    html page recape

--- js/script.js
+++ js/script.js
@@ -109,74 +109,9 @@ const displayBreakingNews = allnews => {
 const homePage = () => {
     fetch('https://openapi.programming-hero.com/api/news/category/01')
         .then(res => res.json())
-        .then(data => newsDisplay(data))
+        .then(data => displayBreakingNews(data))
 }
 homePage();
-
-const newsDisplay = (allnews) => {
-    const newses = allnews.data;
-    newses.sort(function (a, b) {
-        return b.total_view - a.total_view;
-    });
-    const mainContainer = document.getElementById('main-container');
-    mainContainer.innerHTML = '';
-    const sarceRejult = document.getElementById('sarce-rejult').innerText = newses.length;
-    newses.forEach(news => {
-        const maindiv = document.createElement("div");
-        maindiv.classList = ('row my-5');
-        maindiv.innerHTML = `
-                    <div class="col-md-3">
-                        <img src="${news.image_url ? news.image_url : "https://i.ibb.co/g9CSkZQ/image.png "}"
-                            class="img-fluid my-img rounded-start" alt="...">
-                    </div>
-                    <div class="col-md-9">
-                        <div class="card-body">
-                            <h5 class="card-title">${news.title}</h5>
-                            <p class="card-text">${news.details.length > 10 ? news.details.slice(0, 150) : "NO details"}</p>
-                            <p class="card-text">${news.details.length > 150 ? news.details.slice(150, 400) + '...' : "NO details"}</p>
-                            
-                            <div class="d-flex align-items-center justify-content-between mt-5">
-                                <div class="d-flex align-items-center">
-                                    <div>
-                                        <a class="navbar-brand " href="#">
-                                            <img class="rounded-circle"
-                                                src="${news.author.img ? news.author.img : "https://i.ibb.co/g9CSkZQ/image.png "}"
-                                                alt="" width="40" height="40">
-                                        </a>
-                                    </div>
-                                    <div>
-                                        <div>${news.author.name ? news.author.name : "No data"}</div>
-                                        <div>${news.author.published_date ? news.author.published_date : "No data"}</div>
-                                    </div>
-                                </div>
-                                <div class="d-flex align-items-center">
-                                    <div class="h5">
-                                        <i class="fa-regular fa-eye"></i>
-                                    </div>
-                                    <div class="ms-2">
-                                        <h5>${news.total_view ? news.total_view : "No data"}</h5>
-                                    </div>
-                                </div>
-                                <div>
-                                    <i class="fa-solid fa-star-half-stroke"></i>
-                                    <i class="fa-regular fa-star"></i>
-                                    <i class="fa-regular fa-star"></i>
-                                    <i class="fa-regular fa-star"></i>
-                                    <i class="fa-regular fa-star"></i>
-                                </div>
-                                <div>
-                                    <button onclick="modalBodyById('${news._id}')" type="button" class="btn " data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                        <i class="fa-solid fa-arrow-right"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-        `;
-        mainContainer.appendChild(maindiv)
-    })
-}
-
 
 
 const modalBodyById = (id) => {
